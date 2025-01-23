199d0715dd7fd9263bb0973af2fb2ec0c3b1c472
Cudi Yildiz <cudi36y@gmail.com>
2025-01-23 23:36:36 +0300

    proje responsive tasraımı yapıldı

--- script.js
+++ script.js
@@ -36,4 +36,26 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error('Detay bulunamadı:', blockId);
         }
     }
+//iletişim
+// EmailJS kütüphanesini yükle
+(function() {
+    emailjs.init("zO6pAXxLx3Bs9J1nQjiVn"); // Private Key'inizi buraya ekleyin
+})();
+
+// Form gönderildiğinde çalışacak fonksiyon
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Formun otomatik gönderilmesini engelle
+
+    // Form verilerini EmailJS ile gönder
+    emailjs.sendForm('service_1pp0ete', 'template_mkeiwd8', this) // Service ID ve Template ID'yi buraya ekleyin
+        .then(function(response) {
+            alert('Mesajınız başarıyla gönderildi!'); // Başarılı mesajı göster
+            document.getElementById('contact-form').reset(); // Formu temizle
+        }, function(error) {
+            alert('Mesaj gönderilirken bir hata oluştu. Lütfen tekrar deneyin.'); // Hata mesajı göster
+        });
 });
+
+});
+
+
